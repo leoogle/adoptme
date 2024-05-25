2a3c0dd9cdf6fe33b359ebde322c1a7a6e57d4e3
leoogle <leoogle@live.com>
2024-05-25 09:15:08 -0400

    Fetch inner HTML

--- js/eventos.js
+++ js/eventos.js
@@ -18,3 +18,32 @@ fetch('footer.html')
     });
 });
 
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar').innerHTML = data;
+            setupNavLinks();
+            loadContent('main.html');  // Carga el contenido inicial
+        });
+});
+
+function setupNavLinks() {
+    const links = document.querySelectorAll('.nav-link');
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            loadContent(link.getAttribute('href'));
+        });
+    });
+}
+
+function loadContent(url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('content-container').innerHTML = data;
+        });
+}
