81086dd96ed6babacc79fd65b5773eab39e91726
leoogle <leoogle@live.com>
2024-05-25 09:14:21 -0400

    Fetch inner html

--- js/eventos.js
+++ js/eventos.js
@@ -18,22 +18,3 @@ fetch('footer.html')
     });
 });
 
-// Get all navbar links
-const navLinks = document.querySelectorAll('.navbar a');
-
-// Add click event listener to each link
-navLinks.forEach(link => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault(); // Prevent the default link behavior
-
-    // Fetch the content of the linked page
-    fetch(event.target.href)
-      .then(response => response.text())
-      .then(data => {
-        // Update the content container with the fetched data
-        const contentContainer = document.getElementById('content-container');
-        contentContainer.innerHTML = data;
-      })
-      .catch(error => console.error('Error:', error));
-  });
-});
